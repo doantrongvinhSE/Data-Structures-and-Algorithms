cb2e1bbad4a2e12201b2e4e05882b1cf06499967
Doan Trong Vinh <clonevinh95@gmail.com>
2024-10-28 22:52:58 +0700

    done 2pointers

---
+++ src/ArraysAndStrings/TwoPointers/SquareOfASortedArray.java
@@ -0,0 +1,20 @@
+package ArraysAndStrings.TwoPointers;
+
+import java.util.Arrays;
+
+public class SquareOfASortedArray {
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, 0, 3, 10};
+        int[] result = sortedSquares(nums);
+        System.out.println(Arrays.toString(result));
+    }
+    
+    public static int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+
+        Arrays.sort(nums);
+        return nums;
+    }
+}
